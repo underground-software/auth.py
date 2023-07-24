107c6e6fca8e790b20ddbdfc217f37f153e34921
Joel Savitz <jsavitz@redhat.com>
2023-07-24 18:31:39 -0400

    change to script directory in setup
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#
+
 # basic setup of auth server
+
+# change directory to the location of this script
+cd $(dirname $0)
+
 # initialize empty databases
 sqlite3 users.db ".read users.default" ".exit"
 sqlite3 sessions.db ".read sessions.default" ".exit"
 
+# install python dependencies
 pip install -r requirements.txt
