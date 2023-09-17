e2aabc2c9ddc7e822c6ce62202a9b4ecf1636910
Joel Savitz <jsavitz@redhat.com>
2023-09-17 05:03:31 -0400

    setup.sh: ensure script is run in correct directory
    Also, polish output with a couple lines of echo
    
    Signed-off-by: Joel Savitz <jsavitz@redhat.com>

--- setup.sh
+++ setup.sh
@@ -2,7 +2,12 @@
 #
 # basic setup of auth server
 # initialize empty databases
+
+cd $(dirname $0)
+
+echo "Setting up databases..."
 sqlite3 users.db ".read users.default" ".exit"
 sqlite3 sessions.db ".read sessions.default" ".exit"
 
+echo "installing python requirements..."
 pip install -r requirements.txt
